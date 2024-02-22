5944631d3bab0bf5038fe04e2b4bec42a640d6f7
oktayMozaffary <oktay.mozaffari@gmail.com>
2024-02-22 15:55:45 +0300

    cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -6,8 +6,10 @@ import ResultsView from './views/ResultsView.js';
 import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
 import addRecipeView from './views/addRecipeView.js';
+
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import { async } from 'regenerator-runtime';
 
 if (module.hot) {
   module.hot.accept();
